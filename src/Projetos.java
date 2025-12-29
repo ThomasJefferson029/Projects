b1209af41eefa7e1cb9dabfbc6bd664c7d44a280
ThomasJefferson029 <thomas.jefferson.2004l@gmail.com>
2025-12-29 01:22:47 -0300

    Idealizando projeto 1

---
+++ src/Projetos.java
@@ -0,0 +1,7 @@
+public class Projetos {
+
+//Algoritmo capaz de dar o resultado de eficiencia e eficacia de uma producao e classifica-las.
+    public static void main(String[] args){
+        System.out.println("EFICIENCIA E EFICACIA");
+    }
+}
