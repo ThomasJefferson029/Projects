cb8a8498d6f76c3d9e48b9c802388acb2df70f24
ThomasJefferson029 <thomas.jefferson.2004l@gmail.com>
2025-12-29 20:19:00 -0300

    Adicionado Scanners

--- src/Projetos.java
+++ src/Projetos.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Projetos {
 
 //Algoritmo capaz de dar o resultado de eficiencia e eficacia de uma producao e classifica-las.
     public static void main(String[] args){
         System.out.println("EFICIENCIA E EFICACIA");
+
+        //SCANNERS
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escreva sua meta de vendas: ");
+        float meta = sc.nextFloat();
+
+        System.out.print("Escreva sua Produção Total: ");
+        float pt = sc.nextFloat();
+
+        System.out.print("Escreva sua Produção Conforme: ");
+        float pc = sc.nextFloat();
+
+        sc.close();
+
+        System.out.print("Resultado: "+ meta*2);
     }
 }
