fbf66eca9d278efd81a8b9087db525c739a2980e
ThomasJefferson029 <thomas.jefferson.2004l@gmail.com>
2025-12-30 00:31:16 -0300

    Adicionado sistema de classificação

--- src/Projetos.java
+++ src/Projetos.java
@@ -5,6 +5,7 @@ public class Projetos {
 //Algoritmo capaz de dar o resultado de eficiencia e eficacia de uma producao e classifica-las.
     public static void main(String[] args){
         System.out.println("EFICIENCIA E EFICACIA");
+        System.out.println();
 
         //SCANNERS
         Scanner sc = new Scanner(System.in);
@@ -25,7 +26,37 @@ public class Projetos {
         float eficiencia = (pc/pt)*100;
         float eficacia = (pc/mv)*100;
 
+        System.out.println();
         System.out.println("Sua Eficiencia foi de: "+ eficiencia +"%");
         System.out.println("Sua Eficacia foi de: "+ eficacia +"%");
+        System.out.println();
+
+        //Sistema de classificação
+
+        //Transformando em categorias
+        int ef = (eficiencia >= 50) ? 1 : 0;
+        int ec = (eficacia >= 50) ? 1 : 0;
+
+        String situacao;
+
+        //Combinação usando switch
+
+        switch (ef * 10 + ec){
+            case 0:
+                situacao = "Desastroso";
+                break;
+            case 1:
+                situacao = "Oneroso" ;
+                break;
+            case 10:
+                situacao = "Enganoso";
+                break;
+            case 11:
+                situacao = "Ideal" ;
+                break;
+            default:
+                situacao = "Indefinido";
+        }
+        System.out.println("Situação: "+ situacao);
     }
 }
