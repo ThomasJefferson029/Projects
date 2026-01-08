15c979a443ca9fcb8fdf669cc011f0d501cd93af
ThomasJefferson029 <thomas.jefferson.2004l@gmail.com>
2026-01-07 22:06:20 -0300

    Proposta da Atividade

---
+++ src/pratica.java
@@ -0,0 +1,19 @@
+public class pratica {
+/* Atividade Prática
+
+Você foi contratado para desenvolver uma parte de um sistema simples que realiza duas tarefas:
+
+1. Exibir uma saudação personalizada ao usuário.
+2. Calcular e retornar o quadrado de um número inteiro.
+Seu desafio é criar um programa Java que atenda aos seguintes requisitos:
+Requisitos:
+
+1. Crie um metodo void chamado exibirMensagem que:
+- Receba o nome do usuário como parâmetro (tipo String);
+- Imprima uma saudação como: "Olá, [nome]! Seja bem-vindo ao sistema!"
+
+2. Crie um metodo chamado calcularQuadrado que:
+- Receba um número inteiro como parâmetro;
+- Retorne o quadrado desse número (ex: 4 → 16);
+- No main, imprima o resultado da função com uma mensagem explicativa./*
+}
