eac29a619788dfe7b2f9a14fafb8613afbbc2946
ThomasJefferson029 <thomas.jefferson.2004l@gmail.com>
2026-01-07 22:45:08 -0300

    Proposta de solucao 1

--- src/pratica.java
+++ src/pratica.java
@@ -1,28 +1,27 @@
+import java.util.Scanner;
+
 public class pratica {
-/* Atividade Prática
-
-Você foi contratado para desenvolver uma parte de um sistema simples que realiza duas tarefas:
-
-1. Exibir uma saudação personalizada ao usuário.
-2. Calcular e retornar o quadrado de um número inteiro.
-
-Seu desafio é criar um programa Java que atenda aos seguintes requisitos:
-Requisitos:
-
-1. Crie um metodo void chamado exibirMensagem que:
-- Receba o nome do usuário como parâmetro (tipo String);
-- Imprima uma saudação como: "Olá, [nome]! Seja bem-vindo ao sistema!"
-
-2. Crie um metodo chamado calcularQuadrado que:
-- Receba um número inteiro como parâmetro;
-- Retorne o quadrado desse número (ex: 4 → 16);
-- No main, imprima o resultado da função com uma mensagem explicativa.*/
 
     public static void exibirMensagem(String mensagem) {
-        System.out.println("Olá, " + nome + "! Seja bem-vindo ao sistema!");
+        System.out.println(mensagem);
     }
-    public static int quadrado(int a) {
+    public static int calcularQuadrado(int a) {
         return a*a;
     }
 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escreva seu nome de Usuario: ");
+        String nome = sc.nextLine();
+
+        exibirMensagem("Olá, " + nome + "! Seja bem-vindo ao sistema!");
+
+        System.out.println("Escreva o Numero que deseja saber o Quadrado: ");
+        int a = sc.nextInt();
+        sc.close();
+
+        int resultado = calcularQuadrado(a);
+        System.out.println("O resultado do quadrado é: " + resultado);
+    }
 }
